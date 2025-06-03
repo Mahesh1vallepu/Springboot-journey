96924801bf053c62e0ee6558f670ec67b1afc40e
Mahesh Vallepu <maheshvallepu66@gmail.com>
2025-06-03 14:00:30 +0530

    first springboot api

---
+++ firstSpringApi/src/main/java/com/example/firstSpringApi/helloController.java
@@ -0,0 +1,13 @@
+package com.example.firstSpringApi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "Hello World! this is my first Api";
+    }
+}
